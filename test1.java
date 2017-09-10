778663bc406116c0e078c3114e4ddcae489a33dd
ChEnHuI96 <31815014+ChEnHuI96@users.noreply.github.com>
2017-09-10 15:30:27 +0800

    edit test1
    第一次使用github修改文件

--- test1.java
+++ test1.java
@@ -1,5 +1,6 @@
 public class Hello{
-public satic void main(Sting args[]){
-System.out.println("Hello test1");
-}
+      public satic void main(String args[]){
+           System.out.println("Hello test1");
+           System.out.println("Hello test2");
+       }
 }
